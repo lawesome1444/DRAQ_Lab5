5e87f3c80a169056b002be3a17858fc74c3f8f11
Conor ODriscoll <pacman1444@gmail.com>
2023-10-27 09:28:07 +0100

    Improved comments, fixed port assignment

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
+//Waiting for a HTTP request with a get
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//The server will constantly listen for HTTP requests
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
